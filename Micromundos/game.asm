6c2a2a5c3547f28a8a9c9be9bf24c8a529a4ad65
Mauricio Calderon Chavarria <mau141226@gmail.com>
2024-03-05 16:55:14 -0600

    [Update] Some .asm files cleaned

--- Micromundos/game.asm
+++ Micromundos/game.asm
@@ -11,8 +11,8 @@ BGCOLOR		      equ 0ffffh    ;; Etablece el color del fondo en Hexadecimal
 APPLECOLOR        equ 4020h     ;; EStablcer el color de la manzana
 TURTLECOLOR       equ 2040h     ;; Establece el color de la tortuga
 TIMER             equ 046Ch     ;; Establece una direccion de memoria para el timer 
-SNAKEXARRAY       equ 1000h     ;; EStablece la direccion de memoria que guarda las coordenadas X
-SNAKEYARRAY       equ 2000h     ;; EStablece la direccion de memoria que guarda las coordenadas Y
+TURTLEXARRAY       equ 1000h     ;; EStablece la direccion de memoria que guarda las coordenadas X
+TURTLEYARRAY       equ 2000h     ;; EStablece la direccion de memoria que guarda las coordenadas Y
 UP			      equ 0         ;; Establece el vqlor de una constante que representa direccion.
 DOWN		      equ 1         ;; Establece el vqlor de una constante que representa direccion.
 LEFT		      equ 2         ;; Establece el vqlor de una constante que representa direccion.
@@ -21,10 +21,10 @@ SO			      equ 4         ;; Establece el vqlor de una constante que representa d
 SE		          equ 5         ;; Establece el vqlor de una constante que representa direccion.
 NO		          equ 6         ;; Establece el vqlor de una constante que representa direccion.
 NE		          equ 7         ;; Establece el vqlor de una constante que representa direccion.
-UP_DOWN_COLOR     equ 0A000h    ;; EStablece el color del verde hexadecimal
-LEFT_RIGHT_COLOR  equ 0C000h    ;; EStablece el color del rojo hexadecimal
-SO_NE_COLOR		  equ 0010h     ;; EStablece el color del azul hexadecimal
-SE_NO		      equ 0E000h    ;; EStablece el color del amarillo hexadecimall
+GREEN             equ 0A000h    ;; EStablece el color del verde hexadecimal
+RED               equ 0C000h    ;; EStablece el color del rojo hexadecimal
+BLUE		      equ 0010h     ;; EStablece el color del azul hexadecimal
+YELLOW		      equ 0E000h    ;; EStablece el color del amarillo hexadecimal
 
 
 ;; VARIABLES
@@ -32,9 +32,10 @@ playerX:	 dw 40        ;;Variable de tamaño de palabra (16 bits) que almacena l
 playerY:	 dw 12        ;;Variable de tamaño de palabra (12 bits) que almacena la coordenada Y del jugador
 appleX:		 dw 16        ;;Variable de tamaño de palabra (16 bits) que almacena la coordenada X de la manzana
 appleY:		 dw 8         ;;Variable de tamaño de palabra (8 bits) que almacena la coordenada Y de la manzana
-direction:	 db 8         ;;Variable de tamaño de palabra (4 bits) que almacena ldireccion actual
+direction:	 db 8         ;;Variable de tamaño de palabra (8 bits) que almacena ldireccion actual
 snakeLength: dw 1         ;;Variable de tamaño de palabra (1 bits) que almacena la longitud de la serpiente
 printRoad:   db 0         ;;Variable de tamaño de palabra (1 bits) que almacena la flag para pintar el camino
+eraseRoad:   db 0         ;;Variable de tamaño de palabra (1 bits) que almacena la flag para borrar el camino
 
 ;; LOGIC --------------------
 setup_game:
@@ -48,9 +49,9 @@ setup_game:
 
 	;; Set 1st snake segment "head"
 	mov ax, [playerX]
-	mov word [SNAKEXARRAY], ax
+	mov word [TURTLEXARRAY], ax
 	mov ax, [playerY]
-	mov word [SNAKEYARRAY], ax
+	mov word [TURTLEYARRAY], ax
 	
 	;; Hide cursor
 	mov ah, 02h
@@ -70,8 +71,8 @@ game_loop:
 	mov cx, [snakeLength]	; Loop counter
 	mov ax, TURTLECOLOR
 	.turtle_loop:
-		imul di, [SNAKEYARRAY+bx], SCREENW*2	; Y position of snake segment, 2 bytes per character
-		imul dx, [SNAKEXARRAY+bx], 2			; X position of snake segment, 2 bytes per character
+		imul di, [TURTLEYARRAY+bx], SCREENW*2	; Y position of snake segment, 2 bytes per character
+		imul dx, [TURTLEXARRAY+bx], 2			; X position of snake segment, 2 bytes per character
 		add di, dx
         mov word [es:di], ax    ; Coloca el color de la tortuga en la posición de la memoria de video
 
@@ -79,17 +80,17 @@ game_loop:
 		inc bx
 	loop .turtle_loop
 
-	;; Draw apple
-	;;imul di, [appleY], SCREENW*2
-	;;imul dx, [appleX], 2
-	;;add di, dx
-	;;mov ax, APPLECOLOR
-	;;stosw
 
-	;; Move snake in current direction
+	;; Move turtle in current direction
 	mov al, [direction]
     mov si, [playerX]
     mov di, [playerY]
+
+	
+	; Verifica si la flag de borrar esta activa sino se sale del metodo
+	;mov al, [eraseRoad]
+	;cmp al, 1
+	;je set_default_color
 
 	cmp al, UP
 	je move_up
@@ -111,39 +112,79 @@ game_loop:
 	jmp update_snake
 
 	move_up:
+		; Verifica si la flag de pintar esta activa sino se sale del metodo
+		mov al, [printRoad]
+		cmp al, 1
+		je set_up_down_color
 		dec di		; Move up 1 row on the screen
 		jmp update_snake
 
 	move_down:
+		; Verifica si la flag de pintar esta activa sino se sale del metodo
+		mov al, [printRoad]
+		cmp al, 1
+		je set_up_down_color
 		inc di		; Move down 1 row on the screen
 		jmp update_snake
+		
 
 	move_left:
+		; Verifica si la flag de pintar esta activa sino se sale del metodo
+		mov al, [printRoad]
+		cmp al, 1
+		je set_left_right_color
 		dec si		; Move left 1 column on the screen
 		jmp update_snake
+		
 
 	move_right:
+		; Verifica si la flag de pintar esta activa sino se sale del metodo
+		mov al, [printRoad]
+		cmp al, 1
+		je set_left_right_color
 		inc si		; Move right 1 column on the screen
+		jmp update_snake
+		
     
     move_NO:
+		; Verifica si la flag de pintar esta activa sino se sale del metodo
+		mov al, [printRoad]
+		cmp al, 1
+		je set_NO_SE_color
         dec si
         dec di
-        jmp update_snake
+		jmp update_snake
+		
 
     move_SO:
+		; Verifica si la flag de pintar esta activa sino se sale del metodo
+		mov al, [printRoad]
+		cmp al, 1
+		je set_NE_SO_color
         dec si
         inc di
-        jmp update_snake
+		jmp update_snake
+		
 
     move_SE:
+		; Verifica si la flag de pintar esta activa sino se sale del metodo
+		mov al, [printRoad]
+		cmp al, 1
+		je set_NO_SE_color
         inc si
         inc di
-        jmp update_snake
+		jmp update_snake
+		
 
     move_NE:
+		; Verifica si la flag de pintar esta activa sino se sale del metodo
+		mov al, [printRoad]
+		cmp al, 1
+		je set_NE_SO_color
         inc si
         dec di
-        jmp update_snake
+		jmp update_snake
+		
     
 
 	;; Update snake position from playerX/Y changes
@@ -164,59 +205,54 @@ game_loop:
 		;;jnz .turtle_loop							; Stop at first element, "head"
 
 	;; Store updated values to head of snake in arrays
-	mov word [SNAKEXARRAY], si
-	mov word [SNAKEYARRAY], di
+	mov word [TURTLEXARRAY], si
+	mov word [TURTLEYARRAY], di
 	
 	;; Lose conditions
 	;; 1) Hit borders of screen
-	cmp di, -1		; Top of screen
-	je game_lost
-	cmp di, SCREENH	; Bottom of screen
-	je game_lost
-	cmp si, -1		; Left of screen
-	je game_lost
-	cmp si, SCREENW ; Right of screen
-	je game_lost
+	;cmp di, -1		; Top of screen
+	;je game_lost
+	;cmp di, SCREENH	; Bottom of screen
+	;je game_lost
+	;cmp si, -1		; Left of screen
+	;je game_lost
+	;cmp si, SCREENW ; Right of screen
+	;je game_lost
 
-	;; 2) Hit part of snake
-	cmp word [snakeLength], 1	; Only have starting segment
-	je get_player_input
+	; 2) WIN CONDITION 
+	;cmp word [snakeLength], 1	; Only have starting segment
+	;je get_player_input
 
-	mov bx, 2					; Array indexes, start at 2nd array element
-	mov cx, [snakeLength]		; Loop counter
-	check_hit_snake_loop:
-		cmp si, [SNAKEXARRAY+bx]
-		jne .increment
+	;mov bx, 2					; Array indexes, start at 2nd array element
+	;mov cx, [snakeLength]		; Loop counter
+	;check_hit_snake_loop:
+	;		cmp si, [TURTLEXARRAY+bx]
+	;	jne .increment
 
-		cmp di, [SNAKEYARRAY+bx]
-		je game_lost				; Hit snake body, lose game :'(
+	;	cmp di, [TURTLEYARRAY+bx]
+	;	je game_lost				; Hit snake body, lose game :'(
 
-		.increment:
-			inc bx
-			inc bx
-	loop check_hit_snake_loop
+	;	.increment:
+	;		inc bx
+	;		inc bx
+	;loop check_hit_snake_loop
 
 	get_player_input:
 
-
-		mov bl, [direction]		; Save current direction
+		mov bl, [direction]		
 		
-		mov ah, 1
-		int 16h					; Get keyboard status
-		;;jz check_apple			; If no key was pressed, move on
-
+		; Reset a 0 de ah y interrupcion de teclado
 		xor ah, ah
-		int 16h					; Get keystroke, AH = scancode, AL = ascii char entered
+		int 16h					
 
-        cmp ah, 32  
-        je space_pressed
-		cmp ah, 48h      ; Compara con la tecla de flecha arriba
+        
+		cmp ah, 48h      
         je arrow_up_pressed
-        cmp ah, 50h      ; Compara con la tecla de flecha abajo
+        cmp ah, 50h     
         je arrow_down_pressed
-        cmp ah, 4Bh      ; Compara con la tecla de flecha izquierda
+        cmp ah, 4Bh    
         je arrow_left_pressed
-        cmp ah, 4Dh      ; Compara con la tecla de flecha derecha
+        cmp ah, 4Dh      
         je arrow_right_pressed
         cmp al, 'q'
         je q_pressed
@@ -228,87 +264,107 @@ game_loop:
         je d_pressed
         cmp al, 'r'
         je r_pressed
+		cmp al, 'z'
+        je z_pressed
+		cmp ah, 32  
+        je space_pressed
      
 
-		jmp check_apple
+		jmp update_snake
 
         space_pressed:
             ;; Change printRoad status flag
-			mov al, [printRoad]
-            xor al, 1  ; Alternar entre 0 y 1
-            mov [printRoad], al
+			mov ax, [printRoad]
+            xor ax, 1  
+            mov [printRoad], ax
+			jmp update_position
+			
+
+		z_pressed:
+            ;; Change eraseRoad status flag
+			mov ax, [eraseRoad]
+            xor ax, 1  
+            mov [eraseRoad], ax
+			jmp update_position
 
 		arrow_up_pressed:
             ;; Move up
 			mov bl, UP
-			jmp check_apple
+			jmp update_position
 
 		arrow_down_pressed:
             ;; Move down
 			mov bl, DOWN
-			jmp check_apple
+			jmp update_position
 
 		arrow_left_pressed:
             ;; Move left
 			mov bl, LEFT
-			jmp check_apple
+			jmp update_position
 
 		arrow_right_pressed:
             ;; Move right
 			mov bl, RIGHT
-			jmp check_apple
+			jmp update_position
 
         a_pressed:
             ;; Move NO
 			mov bl, NO
-			jmp check_apple
+			jmp update_position
 
 		d_pressed:
             ;; Move NE
 			mov bl, NE
-			jmp check_apple
+			jmp update_position
 
 		q_pressed:
             ;; Move SO
 			mov bl, SO
-			jmp check_apple
+			jmp update_position
 
 		e_pressed:
             ;; Move SE
 			mov bl, SE
-		    	jmp check_apple
+		    jmp update_position
 
 		r_pressed:
             ;; Reset
 			int 19h     ; Reload bootsector
 
-	;; Did player hit apple?
-	check_apple:
+	
+	update_position:
 		mov byte [direction], bl		; Update direction
 		
-		mov ax, si
-		cmp ax, [appleX]
-		jne delay_loop
-
-		mov ax, di
-		cmp ax, [appleY]
-		jne delay_loop
-
-		; Hit apple, increase snake length
-		inc word [snakeLength]
-		cmp word [snakeLength], WINCOND
-		je game_won
-
-	
-	delay_loop:
-		mov bx, [TIMER]
-		inc bx
-		inc bx
-		.delay:
-			cmp [TIMER], bx
-			jl .delay
 
 jmp game_loop
+
+color_draw:
+	; Dibujar el color en la posición actual de la tortuga (playerX, playerY)
+	imul di, [playerY], SCREENW*2
+	imul dx, [playerX], 2
+	add di, dx
+	stosw
+	jmp update_position
+
+set_up_down_color:
+	mov ax, GREEN  
+	jmp color_draw
+
+set_left_right_color:
+	mov ax, RED  
+	jmp color_draw
+
+set_NE_SO_color:
+	mov ax, BLUE  
+	jmp color_draw
+
+set_NO_SE_color:
+	mov ax, YELLOW  
+	jmp color_draw
+
+set_default_color:
+	mov ax, BGCOLOR  
+	jmp color_draw
 
 ;; End conditions
 game_won:
